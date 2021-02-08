6ce67ed7801f5db96ca1a17467ef3e1c78d472ef
Thomas Seufert <tseufert@gmail.com>
2021-02-08 10:24:36 -0500

    cleanup ka

--- kubeapps/install-kubeapps.sh
+++ kubeapps/install-kubeapps.sh
@@ -1,8 +1,10 @@
 set -x
+WORKING_DIR=$(dirname "$0")
+pull_secret=$WORKING_DIR/../common/pull-secret.yaml
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-ytt -f kubeapps-helm-values.yaml -f $1 | helm template bitnami/kubeapps --name-template kubeapps -f- > chart.yaml
+ytt -f $WORKING_DIR/$kubeapps-helm-values.yaml -f $1 | helm template bitnami/kubeapps --name-template kubeapps -f- > $WORKING_DIR/chart.yaml
 
-ytt -f kubeapps-dependencies.yaml -f integrate-contour-overlay.yaml -f chart.yaml -f $1 --file-mark 'chart.yaml:type=yaml-plain' | kapp deploy -a kubeapps -n kubeapps -f- --diff-changes --yes
+ytt -f $WORKING_DIR/kubeapps-dependencies.yaml -f $WORKING_DIR/integrate-contour-overlay.yaml -f $WORKING_DIR/chart.yaml -f $1 -f $pull_secret --file-mark 'chart.yaml:type=yaml-plain' | kapp deploy -a kubeapps -n kubeapps -f- --diff-changes --yes
