8f7b34bc8900baa86bf0e61cdb2849480e84de3d
Thomas Seufert <tseufert@gmail.com>
2021-02-07 23:15:05 -0500

    install

--- certgen/install.sh
+++ certgen/install.sh
@@ -6,3 +6,5 @@ kubectl apply -f
 ytt --ignore-unknown-comments -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yam -f values.yaml -f pull_secret > $WORKING_DIR/certmgr.yml
 
 kapp deploy -a certmgr -n certmgr -f $WORKING_DIR/certmgr.yml --diff-changes --yes
+
+ytt --ignore-unknown-comments -f $WORKING_DIR/issuer.yaml -f values.yaml   kubectl apply -f-
