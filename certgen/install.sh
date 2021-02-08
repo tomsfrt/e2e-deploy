9d71bf10c766ce73db92e88dd5053485ea8cd37b
Thomas Seufert <tseufert@gmail.com>
2021-02-07 23:33:01 -0500

    update

--- certgen/install.sh
+++ certgen/install.sh
@@ -3,7 +3,7 @@ WORKING_DIR=$(dirname "$0")
 pull_secret=$WORKING_DIR/../common/pull-secret.yaml
 
 kubectl apply -f 
-ytt --ignore-unknown-comments -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yam -f values.yaml -f $pull_secret > $WORKING_DIR/certmgr.yml
+ytt --ignore-unknown-comments -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yam -f $pull_secret -f values.yaml  > $WORKING_DIR/certmgr.yml
 
 kapp deploy -a certmgr -n certmgr -f $WORKING_DIR/certmgr.yml --diff-changes --yes
 
