fe2808edf6de4056aaaa106413703726e0989676
Thomas Seufert <tseufert@gmail.com>
2021-02-07 23:42:21 -0500

    update

--- certgen/install.sh
+++ certgen/install.sh
@@ -2,9 +2,9 @@ set -x
 WORKING_DIR=$(dirname "$0")
 pull_secret=$WORKING_DIR/../common/pull-secret.yaml
 
-kubectl --dry-run create ns certmanager -o yaml | kubectl apply -f-
+kubectl create ns certmanager -o yaml --dry-run| kubectl apply -f-
 ytt --ignore-unknown-comments -f https://github.com/jetstack/cert-manager/releases/download/v1.1.0/cert-manager.yam -f $pull_secret -f values.yaml  > $WORKING_DIR/certmgr.yaml
 
-kapp deploy -a certmgr -n certmanager -f $WORKING_DIR/certmgr.yaml --diff-changes --yes
+kapp deploy -a certmanager -n certmanager -f $WORKING_DIR/certmgr.yaml --diff-changes --yes
 
 ytt --ignore-unknown-comments -f $WORKING_DIR/issuer.yaml -f values.yaml | kubectl apply -f-
