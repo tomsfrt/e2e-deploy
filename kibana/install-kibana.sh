295f8108b4bb5966a6184e7e4668098310e9519d
Thomas Seufert <tseufert@gmail.com>
2021-02-10 11:06:30 -0500

    config fluentd

--- kibana/install-kibana.sh
+++ kibana/install-kibana.sh
@@ -5,4 +5,5 @@ pull_secret=$WORKING_DIR/../common/pull-secret.yaml
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
+#cheating here a bit, kapp problems with helm template and test pods
 ytt -f $1 -f $WORKING_DIR/chart.yaml -f $pull_secret --ignore-unknown-comments | kapp deploy -a kibana -n kibana -f- --diff-changes --yes 
