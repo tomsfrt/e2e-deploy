40e67c171da1382b854ce2457822d63753e75b70
Thomas Seufert <tseufert@gmail.com>
2021-02-10 13:44:52 -0500

    different approach to fluentd

--- fluentd/install-fluentd.sh
+++ fluentd/install-fluentd.sh
@@ -11,7 +11,6 @@ helm template \
 --set aggregator.extraEnv[0].name=9200 \
 --include-crds bitnami/fluentd \
 -n fluentd \
---name-template fluentd \
--f- > $WORKING_DIR/chart.yaml
+--name-template fluentd  > $WORKING_DIR/chart.yaml
 
 ytt -f $WORKING_DIR/fluentd-dependencies.yaml -f $1 -f $WORKING_DIR/chart.yaml -f $pull_secret --file-mark 'chart.yaml:type=yaml-plain' | kapp deploy -a fluentd -n fluentd -f- --diff-changes --yes
