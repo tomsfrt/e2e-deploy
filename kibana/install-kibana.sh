352d97dfa8321e539b9d24589db2a6dd026d60bf
Thomas Seufert <tseufert@gmail.com>
2021-02-10 09:21:18 -0500

    remove test pod

--- kibana/install-kibana.sh
+++ kibana/install-kibana.sh
@@ -7,4 +7,4 @@ helm repo update
 
 ytt -f $WORKING_DIR/kibana-helm-values.yaml -f $1 | helm template --include-crds bitnami/kibana -n kibana --name-template kibana -f- > $WORKING_DIR/chart.yaml
 
-ytt -f $WORKING_DIR/kibana-dependencies.yaml -f $1 -f $WORKING_DIR/chart.yaml -f $pull_secret --file-mark 'chart.yaml:type=yaml-plain' | kapp deploy -a kibana -n kibana -f- --diff-changes --yes
+ytt -f $WORKING_DIR/kibana-dependencies.yaml -f $1 -f $WORKING_DIR/chart.yaml -f $pull_secret --file-mark 'chart.yaml:type=yaml-plain' | kapp deploy -a kibana -n kibana -f- --diff-changes --yes --filter-kind-name pod/kibana-connection-test
