c43ebed7ea455e1e473672d57ec873e4bb0e84f3
Thomas Seufert <tseufert@gmail.com>
2021-02-10 09:54:12 -0500

    kibana test

--- kibana/install-kibana.sh
+++ kibana/install-kibana.sh
@@ -5,4 +5,4 @@ pull_secret=$WORKING_DIR/../common/pull-secret.yaml
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-ytt -f $WORKING_DIR/kibana-dependencies.yaml -f $1 -f $WORKING_DIR/chart.yaml -f $pull_secret --file-mark 'chart.yaml:type=yaml-plain' | kapp deploy -a kibana -n kibana -f- --diff-changes --yes --filter-kind-name pod/kibana-test-connection
+ytt -f $1 -f $WORKING_DIR/chart.yaml -f $pull_secret --ignore-unknown-comments | kapp deploy -a kibana -n kibana -f- --diff-changes --yes 
