332f2f80e08abd2beb65bbcecf77604e0d9f9bf4
Thomas Seufert <tseufert@gmail.com>
2021-02-10 12:41:26 -0500

    different approach to fluentd

--- fluentd/install-fluentd.sh
+++ fluentd/install-fluentd.sh
@@ -5,6 +5,13 @@ pull_secret=$WORKING_DIR/../common/pull-secret.yaml
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-ytt -f $WORKING_DIR/fluentd-helm-values.yaml -f $1 | helm template --include-crds bitnami/fluentd -n fluentd --name-template fluentd -f- > $WORKING_DIR/chart.yaml
+ytt -f $WORKING_DIR/fluentd-helm-values.yaml -f $1 | helm template \
+--set aggregator.configMap=elasticsearch-output \
+--set aggregator.extraEnv[0].name=elasticsearch-elasticsearch-data.elasticsearch.svc.cluster.local \
+--set aggregator.extraEnv[1].name=9200 \
+--include-crds bitnami/fluentd \
+-n fluentd \
+ --name-template fluentd \
+ -f- > $WORKING_DIR/chart.yaml
 
 ytt -f $WORKING_DIR/fluentd-dependencies.yaml -f $1 -f $WORKING_DIR/chart.yaml -f $pull_secret --file-mark 'chart.yaml:type=yaml-plain' | kapp deploy -a fluentd -n fluentd -f- --diff-changes --yes
