364161ac3454ca14d00b4cb8f82e0fe5397a6b66
Thomas Seufert <tseufert@gmail.com>
2021-02-10 14:16:25 -0500

    different approach to fluentd

--- fluentd/install-fluentd.sh
+++ fluentd/install-fluentd.sh
@@ -6,11 +6,11 @@ helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
 helm template \
---set aggregator.configMap="elasticsearch-output" \
+--set aggregator.configMap=elasticsearch-output \
 --set aggregator.extraEnv[0].name=ELASTICSEARCH_HOST \
---set aggregator.extraEnv[0].value="elasticsearch-elasticsearch-data.elasticsearch.svc.cluster.local" \
+--set aggregator.extraEnv[0].value=elasticsearch-elasticsearch-data.elasticsearch.svc.cluster.local \
 --set aggregator.extraEnv[1].name=ELASTICSEARCH_PORT \
---set aggregator.extraEnv[1].value="9200" \
+--set aggregator.extraEnv[1].value=9200 \
 --include-crds bitnami/fluentd \
 -n fluentd \
 --name-template fluentd  > $WORKING_DIR/chart.yaml
