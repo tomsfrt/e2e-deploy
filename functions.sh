eb59563ffe9edf0cea772bed76b68e8ab8ed1abd
Thomas Seufert <tseufert@gmail.com>
2021-02-09 19:53:59 -0500

    change db config

--- functions.sh
+++ functions.sh
@@ -28,5 +28,5 @@ create_db() {
   export MYSQL_REGISTRY=$dockerhub
   helm repo add bitnami https://charts.bitnami.com/bitnami
   helm repo update
-  helm upgrade -i petclinic-db bitnami/mysql -n $namespace --version 6.14.11 -f <(cat petclinic-db-values.yaml | envsubst)
+  helm upgrade -i petclinic-db bitnami/mysql -n $namespace --version 6.14.11 -f <(cat db-values.yaml | envsubst)
 }
